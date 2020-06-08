7abda3450db11e8ca3e976892a1ea68932abe7fc
Daniel Hahm <dhahm01@gmail.com>
2020-06-08 18:28:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,16 @@
 def using_concat(array, array2)
   array.concat(array2)
 end
+
+def using_insert(array, string)
+  array.insert(string)
+end 
+
+def using_uniq(array)
+ array.uniq()
+end
+
+def using_flatten(array, array2)
+  array.flatten(array2)
+end
+
